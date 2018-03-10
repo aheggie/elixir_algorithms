da20a4bdc564e8645992390cb2cdcb055b9b87cd
Alex <git@alexheggie.net>
2018-03-10 15:36:56 -0500

    Merge Sort started

---
+++ merge_sort/lib/mergesort.ex
@@ -0,0 +1,18 @@
+defmodule Mergesort do
+  @moduledoc """
+  Documentation for Mergesort.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mergesort.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ merge_sort/test/mergesort_test.exs
@@ -0,0 +1,8 @@
+defmodule MergesortTest do
+  use ExUnit.Case
+  doctest Mergesort
+
+  test "greets the world" do
+    assert Mergesort.hello() == :world
+  end
+end
