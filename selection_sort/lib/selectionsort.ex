a0637ec6b0a9110de4b5741ed5410a26969f0bb2
Alex <git@alexheggie.net>
2018-03-14 00:29:15 -0400

    started selection sort

---
+++ selection_sort/lib/selectionsort.ex
@@ -0,0 +1,18 @@
+defmodule Selectionsort do
+  @moduledoc """
+  Documentation for Selectionsort.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Selectionsort.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ selection_sort/test/selectionsort_test.exs
@@ -0,0 +1,8 @@
+defmodule SelectionsortTest do
+  use ExUnit.Case
+  doctest Selectionsort
+
+  test "greets the world" do
+    assert Selectionsort.hello() == :world
+  end
+end
