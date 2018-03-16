8ec22b6c8dc58c18e2ea95a625ffd7f9d44d640d
Alex <git@alexheggie.net>
2018-03-16 18:00:07 -0400

    README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+I'm implementing versions of various algorithms in Elixir as theoretical practice with the language.
+
+Starting with the algorithm's described in Rob Conery's Imposter's Handbook and then maybe others.
--- selection_sort/lib/selectionsort.ex
+++ selection_sort/lib/selectionsort.ex
@@ -1,15 +1,28 @@
 defmodule SelectionSort do
+
+  @doc """
+  SelectionSort.sort/1 takes an unsorted list of numbers and returns that list sorted in
+  ascending order
+  """
+  
+  @spec sort(list()) :: list()
+  
   def sort(list) do
     sort(list, [])
   end
+  #entry point
 
   defp sort([], results) do
     results
   end
+  #exit point
 
   defp sort(list, results) do
     {min, remaining} = List.pop_at(list, Which.which(:max, list))
     #using max rather than min means we don't need to Enum.reverse the final results
+    #Note: index-based at functions are linear time in elixir
     sort(remaining, [min | results])
   end
+  #recursion
+
 end
