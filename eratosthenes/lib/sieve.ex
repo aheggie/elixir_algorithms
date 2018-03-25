195333d28b97028c02b02b6e8787a836154f9d0c
Alex <git@alexheggie.net>
2018-03-25 18:00:14 -0400

    Sieve of Eratosthenes working

--- eratosthenes/lib/sieve.ex
+++ eratosthenes/lib/sieve.ex
@@ -4,15 +4,24 @@ defmodule Sieve do
   """
 
   @doc """
-  Hello world.
-
   ## Examples
 
-      iex> Sieve.hello
-      :world
+      iex> Sieve.primes_up_to(10)
+      [2,3,5,7]
 
   """
-  def hello do
-    :world
+  def primes_up_to(n) do
+    primes_up_to(Enum.to_list(2..n), [])
+  end
+
+  defp primes_up_to([], result) do
+    result
+    |> Enum.reverse
+  end
+
+  defp primes_up_to([new_prime | tail], result) do
+    tail
+    |> Enum.filter(fn(item) -> rem(item, new_prime) != 0 end)
+    |> primes_up_to([new_prime | result])
   end
 end
