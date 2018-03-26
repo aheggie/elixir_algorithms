7c1a51d28ba9970d213fa97ff73e6f7817752dd5
Alex <git@alexheggie.net>
2018-03-25 22:05:49 -0400

    Sieve commented and doctests

--- eratosthenes/lib/sieve.ex
+++ eratosthenes/lib/sieve.ex
@@ -1,15 +1,22 @@
 defmodule Sieve do
   @moduledoc """
-  Documentation for Sieve.
+  Implementation of the sieve of Eratosthenes.
   """
 
   @doc """
+  Sieve.primes_up_to/1 takes some positive integer n
+  and returns a list of all primes from 2 until n .
+
   ## Examples
 
       iex> Sieve.primes_up_to(10)
       [2,3,5,7]
 
   """
+
+  @spec primes_up_to(integer()) :: list()
+
+
   def primes_up_to(n) do
     primes_up_to(Enum.to_list(2..n), [])
   end
@@ -21,7 +28,14 @@ defmodule Sieve do
 
   defp primes_up_to([new_prime | tail], result) do
     tail
-    |> Enum.filter(fn(item) -> rem(item, new_prime) != 0 end)
+    |> remove_multiples_of(new_prime)
+    # This filters out all multiples of the newly identified prime
     |> primes_up_to([new_prime | result])
+  end
+
+  defp remove_multiples_of(list, factor) do
+    Enum.filter(list, fn(item) -> rem(item, factor) != 0 end)
+    #note rem/2 is short for 'remainder' not 'remove'
+    #i.e. it is the modulus function
   end
 end
--- eratosthenes/test/sieve_test.exs
+++ eratosthenes/test/sieve_test.exs
@@ -1,8 +1,4 @@
 defmodule SieveTest do
   use ExUnit.Case
   doctest Sieve
-
-  test "greets the world" do
-    assert Sieve.hello() == :world
-  end
 end
